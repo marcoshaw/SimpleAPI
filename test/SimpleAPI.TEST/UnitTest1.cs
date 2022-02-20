52c01479fbdc4a7b75814a4e6deee65d531ba546
Marco Shaw <marco.shaw@gmail.com>
2022-02-20 15:52:55 -0400

    Initial Commit

---
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.TEST;
+
+public class UnitTest1
+{
+
+WeatherForecastController controller=new WeatherForecastController();
+[Fact]
+public void GetReturnsMyName()
+{
+    var returnValue=controller.Get(1);
+    Assert.Equal("Marco Shaw", returnValue.value);
+}
+
+
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
